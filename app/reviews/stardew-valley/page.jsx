3d7c812a8ae82fda2f5f77e22349523a544511de
Oleksandr Grymashevych <alexgrimm87@gmail.com>
2024-02-03 15:49:55 +0200

    install and implement tailwind typography, marked and gray-matter for render markdown files

--- app/reviews/stardew-valley/page.jsx
+++ app/reviews/stardew-valley/page.jsx
@@ -1,15 +1,19 @@
+import {readFile} from 'node:fs/promises';
+import matter from "gray-matter";
+import {marked} from "marked";
 import Heading from "@/components/Heading";
 
-export default function StardewValleyPage() {
+export default async function StardewValleyPage() {
+  const text = await readFile('./content/reviews/stardew-valley.md', 'utf8');
+  const { content, data: { title, date, image } } = matter(text);
+  const html = marked(content);
+
   return (
     <>
-      <Heading>Stardew Valley</Heading>
-      <img src="/images/stardew-valley.jpg" alt=""
-           width="640" height="360" className="mb-2 rounded"
-      />
-      <p>
-        This will be the review for Stardew Valley.
-      </p>
+      <Heading>{title}</Heading>
+      <p className="italic pb-2">{date}</p>
+      <img src={image} alt="" width="640" height="360" className="mb-2 rounded" />
+      <article dangerouslySetInnerHTML={{ __html: html }} className="max-w-screen-sm prose prose-slate" />
     </>
   );
 }
