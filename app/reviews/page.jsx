ec848b6185124a58abb32e687fd844490984522a
Oleksandr Grymashevych <alexgrimm87@gmail.com>
2024-02-03 17:36:06 +0200

    add metadata and icon

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -2,6 +2,10 @@ import Link from "next/link";
 import {getReviews} from "@/lib/reviews";
 import Heading from "@/components/Heading";
 
+export const metadata = {
+  title: 'Reviews',
+};
+
 export default async function ReviewsPage() {
   const reviews = await getReviews();
 
