9a64e34e9b55cc54ae315235dd5157b2ee3b14e9
Oleksandr Grymashevych <alexgrimm87@gmail.com>
2024-02-06 11:46:09 +0200

    environment variables

--- next.config.js
+++ next.config.js
@@ -6,12 +6,24 @@ module.exports = {
     //loader: 'custom',
     //loaderFile: 'my-loader.js',
     remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '1337',
-        pathname: '/uploads/**',
-      },
+      // {
+      //  protocol: 'http',
+      //  hostname: 'localhost',
+      //  port: '1337',
+      //  pathname: '/uploads/**',
+      // },
+      toRemotePattern(process.env.CMS_IMAGE_PATTERN),
     ],
   },
 };
+
+function toRemotePattern(urlString) {
+  const url = new URL(urlString);
+
+  return {
+    protocol: url.protocol.replace(':', ''),
+    hostname: url.hostname,
+    port: url.port,
+    pathname: url.pathname,
+  };
+}
