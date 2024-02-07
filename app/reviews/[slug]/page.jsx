90f37ce6593abf25f245576b6b89995a07b5f01d
Oleksandr Grymashevych <alexgrimm87@gmail.com>
2024-02-07 18:45:18 +0200

    review page add comment form and list

--- app/reviews/[slug]/page.jsx
+++ app/reviews/[slug]/page.jsx
@@ -1,8 +1,11 @@
 import Image from 'next/image';
+import {ChatBubbleBottomCenterTextIcon} from '@heroicons/react/24/outline';
+import {notFound} from "next/navigation";
 import {getReview, getSlugs} from "@/lib/reviews";
 import Heading from "@/components/Heading";
 import ShareLinkButton from "@/components/ShareLinkButton";
-import {notFound} from "next/navigation";
+import CommentForm from "@/components/CommentForm";
+import CommentList from "@/components/CommentList";
 
 export async function generateStaticParams() {
   const slugs = await getSlugs();
@@ -38,6 +41,14 @@ export default async function ReviewPage({ params: { slug } }) {
       </div>
       <Image src={review.image} alt="" priority width="640" height="360" className="mb-2 rounded" />
       <article dangerouslySetInnerHTML={{ __html: review.body }} className="max-w-screen-sm prose prose-slate" />
+      <section className="border-dashed border-t max-w-screen-sm mt-3 py-3">
+        <h2 className="font-bold flex gap-2 items-center text-xl">
+          <ChatBubbleBottomCenterTextIcon className="h-6 w-6" />
+          Comments
+        </h2>
+        <CommentForm title={review.title} />
+        <CommentList />
+      </section>
     </>
   );
 }
